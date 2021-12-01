5cef8f5c79ab23f8068d2f9df20d265ced872ec0
Henri Bresteau <henri.bresteau@gmail.com>
2021-12-01 16:14:02 +0100

    theme variant heading

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Main from "../Component/Layouts/main";
+import theme from "../lib/theme";
 import "../styles/globals.css";
 
 function Website({ Component, pageProps, router }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Main>
         <Component {...pageProps} />
       </Main>
