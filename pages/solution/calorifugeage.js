d04712bd650d589eec6e8633b60f6c0cfc2944a2
Henri Bresteau <henri.bresteau@gmail.com>
2021-12-07 17:03:06 +0100

    Page calo

---
+++ pages/solution/calorifugeage.js
@@ -0,0 +1,12 @@
+import { Container, Heading } from "@chakra-ui/react";
+import Layout from "../../Component/Layouts/articles";
+import { Title } from "../../Component/Solutions/solutions";
+
+const Solution = () => (
+  <Layout title="calorifugeage">
+    <Container>
+      <Title>Calorifugeage</Title>
+    </Container>
+  </Layout>
+);
+export default Solution;
