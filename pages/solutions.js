4c64f427d535cb809e34c1caae3853e68403f034
Henri Bresteau <henri.bresteau@gmail.com>
2021-12-07 17:04:06 +0100

    page solution

---
+++ pages/solutions.js
@@ -0,0 +1,20 @@
+import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
+import Layout from "../Component/Layouts/articles";
+import Section from "../Component/Layouts/section";
+
+const Solutions = () => (
+  <Layout title="solutions">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Nos solutions
+      </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>Colorifugeage</Section>
+        <Section>Points Singulier</Section>
+        <Section>Pompes à chaleur</Section>
+      </SimpleGrid>
+    </Container>
+  </Layout>
+);
+export default Solutions;
