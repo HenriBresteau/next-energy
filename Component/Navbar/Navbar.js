e323fad196894f658c1ba17c2a5304dcdc7866b2
Henri Bresteau <henri.bresteau@gmail.com>
2021-12-14 11:49:57 +0100

    correction Popover

--- Component/Navbar/Navbar.js
+++ Component/Navbar/Navbar.js
@@ -63,22 +63,21 @@ export const Navbar = () => {
         >
           {navItems.map((navItem) => (
             <Box key={navItem.label}>
-              <Popover trigger={"hover"} placement="bottom-start">
-                {console.log(navItem.href)}
-                <NextLink href={navItem.href} passHref>
-                  <PopoverTrigger>
-                    <Link
-                      p={2}
-                      _hover={{
-                        textDecoration: "none",
-                        color: "#8dc449",
-                      }}
-                    >
-                      {navItem.label}
-                    </Link>
-                  </PopoverTrigger>
-                </NextLink>
-                {navItem.children && (
+              {navItem.children ? (
+                <Popover trigger={"hover"} placement="bottom-start">
+                  <NextLink href={navItem.href} passHref>
+                    <PopoverTrigger>
+                      <Link
+                        p={2}
+                        _hover={{
+                          textDecoration: "none",
+                          color: "#8dc449",
+                        }}
+                      >
+                        {navItem.label}
+                      </Link>
+                    </PopoverTrigger>
+                  </NextLink>
                   <PopoverContent
                     border={0}
                     boxShadow={"xl"}
@@ -92,8 +91,20 @@ export const Navbar = () => {
                       ))}
                     </Stack>
                   </PopoverContent>
-                )}
-              </Popover>
+                </Popover>
+              ) : (
+                <NextLink href={navItem.href} passHref>
+                  <Link
+                    p={2}
+                    _hover={{
+                      textDecoration: "none",
+                      color: "#8dc449",
+                    }}
+                  >
+                    {navItem.label}
+                  </Link>
+                </NextLink>
+              )}
             </Box>
           ))}
         </Stack>
@@ -121,26 +132,27 @@ export const Navbar = () => {
 };
 export default Navbar;
 const DesktopNav = () => {
-  const linkColor = useColorModeValue('gray.600', 'gray.200');
-  const linkHoverColor = useColorModeValue('gray.800', 'white');
-  const popoverContentBgColor = useColorModeValue('white', 'gray.800');
+  const linkColor = useColorModeValue("gray.600", "gray.200");
+  const linkHoverColor = useColorModeValue("gray.800", "white");
+  const popoverContentBgColor = useColorModeValue("white", "gray.800");
 
   return (
-    <Stack direction={'row'} spacing={4}>
+    <Stack direction={"row"} spacing={4}>
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label}>
-          <Popover trigger={'hover'} placement={'bottom-start'}>
+          <Popover trigger={"hover"} placement={"bottom-start"}>
             <PopoverTrigger>
               <Link
                 p={2}
-                href={navItem.href ?? '#'}
-                fontSize={'sm'}
+                href={navItem.href ?? "#"}
+                fontSize={"sm"}
                 fontWeight={500}
                 color={linkColor}
                 _hover={{
-                  textDecoration: 'none',
+                  textDecoration: "none",
                   color: linkHoverColor,
-                }}>
+                }}
+              >
                 {navItem.label}
               </Link>
             </PopoverTrigger>
@@ -148,11 +160,12 @@ const DesktopNav = () => {
             {navItem.children && (
               <PopoverContent
                 border={0}
-                boxShadow={'xl'}
+                boxShadow={"xl"}
                 bg={popoverContentBgColor}
                 p={4}
-                rounded={'xl'}
-                minW={'sm'}>
+                rounded={"xl"}
+                minW={"sm"}
+              >
                 <Stack>
                   {navItem.children.map((child) => (
                     <DesktopSubNav key={child.label} {...child} />
@@ -296,6 +309,7 @@ const MobileNavItem = ({ label, children, href }) => {
   );
 };
 const navItems = [
+  { label: "CEE", href: "/CEE" },
   {
     label: "Solutions",
     href: "/solutions",
