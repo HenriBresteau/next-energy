e3abf7d52ad831529fa293493be6520a134c05e8
Henri Bresteau <henri.bresteau@gmail.com>
2021-12-15 14:02:38 +0100

    add SpanCeeTitle

--- Component/Vous-etes/cee.js
+++ Component/Vous-etes/cee.js
@@ -1,7 +1,14 @@
 import styled from "@emotion/styled";
 
-const SpanCee = styled.span`
+export const SpanCee = styled.span`
   color: #1ab107;
   font-weight: 700;
 `;
-export default SpanCee;
+
+export const SpanCeeTitle = styled.span`
+  color: #1ab107;
+  font-weight: 800;
+  text-decoration: underline;
+  text-decoration-color: #1ab107;
+  text-decoration-thickness: 2;
+`;
