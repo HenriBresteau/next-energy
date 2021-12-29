1d35a40209b0d6f3486baf6648500d3a2896a79d
Henri Bresteau <henri.bresteau@gmail.com>
2021-12-29 09:41:43 +0100

    warning construction

--- pages/references.js
+++ pages/references.js
@@ -1,3 +1,4 @@
+import { WarningTwoIcon } from "@chakra-ui/icons";
 import { Box, Text, Container, Heading } from "@chakra-ui/react";
 import Layout from "../Component/Layouts/articles";
 
@@ -10,21 +11,12 @@ export default function References() {
             Références
           </Heading>
           <Box>
-            <Heading as="h3" pb={4}>
-              Ils nous font confiances :
-            </Heading>
-            <Text pb={2}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              vitae aliquet risus. Donec congue lorem neque, ac scelerisque
-              risus tempus eu. Praesent vitae venenatis purus, vitae ullamcorper
-              lorem. Aenean sit amet consectetur felis. Donec augue arcu,
-              ultricies eget auctor ut, aliquam sed tellus. Pellentesque
-              habitant morbi tristique senectus et netus et malesuada fames ac
-              turpis egestas. Donec mollis condimentum massa sit amet
-              pellentesque. Pellentesque sed eleifend tellus, quis laoreet nunc.
-              Mauris semper odio ut tortor lobortis semper id at ligula. Sed
-              vitae vehicula felis.
-            </Text>
+            <Box textAlign="center" py={10} px={6}>
+              <WarningTwoIcon boxSize={"50px"} color={"orange.300"} />
+              <Heading as="h3" pb={4} mt={6}>
+                Page en cours de construction
+              </Heading>
+            </Box>
           </Box>
         </Box>
       </Container>
