f3da79e5449ed767d38b1040b72cd968995ce52e
Henri Bresteau <henri.bresteau@gmail.com>
2021-12-29 11:40:19 +0100

    pb 16

--- Component/Layouts/main.js
+++ Component/Layouts/main.js
@@ -23,7 +23,7 @@ export default function Main({ children, router }) {
       <Container
         className={styles.main}
         pt={{ base: "220px", md: "160px" }}
-        pb={8}
+        pb={16}
         maxW="container.xl"
       >
         {children}
